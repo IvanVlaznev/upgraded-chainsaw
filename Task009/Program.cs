cec0cbfe7c5241543d58e98c24a676e34a825655
Ivan Vlaznev <vlaznev.beatongroup.ru>
2022-09-06 21:34:34 +0300

    add task 009

---
+++ Task009/Program.cs
@@ -0,0 +1,6 @@
+﻿int num = new Random().Next(10, 100);
+int firstDigit = num / 10;
+int secondDigit = num % 10;
+
+if(firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {num} -> {firstDigit}");
+else Console.WriteLine($"Наибольшая цифра числа {num} -> {secondDigit}");
