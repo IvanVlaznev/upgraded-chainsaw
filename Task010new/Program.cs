d07b233c82ff23b837d3e94345a536107acf29cf
Ivan Vlaznev <vlaznev.beatongroup.ru>
2022-09-15 08:19:18 +0300

    add Task 010

---
+++ Task010new/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.Write("Введите трёхначное число: ");
+int three_DigitNumber = Convert.ToInt32(Console.ReadLine());
+string stringNumber = Convert.ToString(three_DigitNumber);
+Console.WriteLine("вторая цифра этого числа -> "+stringNumber[1]);
